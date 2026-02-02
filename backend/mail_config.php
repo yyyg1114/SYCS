8f1ceda44de99d0954eb2fc6becb7f2d73a2002a
yyyg1114 <pkyg3328@gmail.com>
2026-02-02 10:04:21 +0900

    v1.0.61

--- backend/mail_config.php
+++ backend/mail_config.php
@@ -8,9 +8,9 @@ return [
     'host'       => 'smtp.gmail.com',         // SMTPサーバーのホスト名
     'auth'       => true,                     // SMTP認証を有効にするか
     'username'   => 'pkyg3328@gmail.com',   // メールアドレス
-    'password'   => 'zwmr pbxl jxea kpkf',      // アプリパスワード（通常のパスワードではなく）
+    'password'   => 'axvx sqhv ogrj wkbe',      // アプリパスワード（通常のパスワードではなく）
     'secure'     => 'tls',                    // 暗号化方式 (tls または ssl)
     'port'       => 587,                      // ポート番号 (TLSは587, SSLは465が一般的)
-    'from_email' => 'pkyg3328@gmail.com',   // 送信元メールアドレス
+    'from_email' => 'information.sycs@gmail.com',   // 送信元メールアドレス
     'from_name'  => 'SYCS Administrator',     // 送信者名
 ];
